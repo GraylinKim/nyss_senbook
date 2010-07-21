b1278fa9ee0445ac02f74875ba27989c47eac89e
Graylin Kim <graylin.kim@gmail.com>
2010-07-21 10:55:30 -0400

    Added a wrapper/adapter class for the ldap module to abstract its logic out of the handlers and give it a more pythonic interface (generators).

---
+++ ldapter.py
@@ -0,0 +1,54 @@
+import ldap
+import settings
+
+class ldaptor:
+    
+    methods = ['simple','sasl']
+    def __init__(self,url,**kwargs):
+        defaults = dict( who='',cred='',method='simple' )
+        options = dict(defaults.items()+kwargs.items())
+        
+        if options['method'] not in self.methods:
+            raise ValueError(msg % (options['method'],self.methods))
+            
+        self.__dict__.update(options.items()+[['url',url]])
+        
+    def search(self,filterstr="(objectClass=*)",**kwargs):
+        options = dict(self.__dict__.items()+kwargs.items())
+        base = options.get('base',"")
+        scope = options.get('scope',ldap.SCOPE_SUBTREE)
+        filterstr = filterstr
+        attrlist = options.get('attrlist',None)
+        attrsonly = options.get('attrsonly',0)
+        timeout = options.get('timeout', -1)
+        all = options.get('all',0)
+        
+        search_id = self.server.search(base,scope,filterstr,attrlist,attrsonly)
+        result_type = 0
+        
+        result_type = -1
+        while result_type != ldap.RES_SEARCH_RESULT:
+            result_type, result_data = self.server.result(search_id, all=all,timeout=timeout)
+            for result in result_data:
+                yield result
+
+    def connect(self,url,who='',cred='',method='simple',**kwargs):
+        
+        self.server = ldap.initialize(url)
+        if method == 'simple':
+            self.server.simple_bind_s(who,cred)
+        elif method == 'sasl':
+            raise NotImplementedError("SASL authentication not yet implemented")
+        
+        return self
+        
+if __name__ == '__main__':
+    
+    ldap_config = dict(
+        url='ldap://webmail.senate.state.ny.us/',
+        who='',
+        cred='',
+        auth='simple', #Allowed values are ['simple','sasl']
+        )
+        
+    l = ldaptor(**ldap_config)
